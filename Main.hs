96f3f10e83ecceb53fb69031d5c6c10322df2c1c
Goshi Shinozaki <shinozaki54@gmail.com>
2022-09-24 11:49:43 +0000

    add cpu mode

--- Main.hs
+++ Main.hs
@@ -1,47 +1,93 @@
 import Data.Char
+import Data.List
 import Data.Time.Clock.POSIX
 
 type Ans = [Char]
 type Hint = (Int, Int)
 
+shuffle :: Eq a => [a] -> Int -> [a]
+shuffle [] _ = []
+shuffle xs g = y : shuffle ys g
+  where
+    n = g `mod` (length xs)
+    y = xs !! n
+    ys = delete y xs
+
 cpuAns :: IO Ans
 cpuAns = do
-  ms <- (round . (* 1000)) <$> getPOSIXTime
-  return $ take 4 (shuffle ['0'..'9'] ms)
+  g <- round <$> getPOSIXTime
+  return $ take 4 (shuffle ns g)
   where
-    shuffle [] _ = []
-    shuffle xs g = 
-      let n = g `mod` (length xs - 1)
-          xs' = (take n xs) ++ drop (n + 1) xs
-      in (xs !! n) : shuffle xs' g
+    ns = ['0'..'9']
 
-ensureAns :: String -> Maybe Ans
-ensureAns str
-  | not4Digit str = Nothing
-  | notNumber str = Nothing
-  | notUnique str = Nothing
-  | otherwise     = Just str
+parseAns :: String -> Maybe Ans
+parseAns s
+  | not4Digit = Nothing
+  | notNumber = Nothing
+  | notUnique = Nothing
+  | otherwise = Just s
   where
-    f x = notElem x ['0'..'9']
-    not4Digit s = length s /= 4
-    notNumber s = any f s
-    notUnique s = 
-      s /= [x | x <- s, y <- s, x == y]
+    not4Digit = length s /= 4
+    notNumber = any (not . isDigit) s
+    notUnique = s /= nub s
+
+parseHint :: [String] -> Maybe Hint
+parseHint ss
+  | length ss /= 2 = Nothing
+  | or [length x /= 1 | x <- ss] = Nothing
+  | or [notElem x "01234" | x <- cs] = Nothing
+  | otherwise = checkHint cs
+  where cs = map head ss
+
+checkHint :: [Char] -> Maybe Hint
+checkHint cs = 
+  let (h:b:_) = map digitToInt cs
+  in if (h + b > 4) then Nothing
+                    else Just (h, b)
 
 playerAns :: IO Ans
 playerAns = do
   s <- getLine
-  case ensureAns s of
+  case parseAns s of
     Nothing -> do
       putStrLn "Invalid input."
       playerAns
     Just a -> return a
 
+playerHint :: IO Hint
+playerHint = do
+  putStr "HIT BLOW = "
+  ss <- words <$> getLine
+  case parseHint ss of
+    Nothing -> do
+      putStrLn "Invalid input."
+      playerHint
+    Just a -> return a
+
+choiceAns :: [Ans] -> IO Ans
+choiceAns xs = do
+  g <- round <$> getPOSIXTime
+  let n = g `mod` (length xs)
+  return $ xs !! n
+
+possibleAns :: [Ans]
+possibleAns = permut 4 ['0'..'9']
+
+permut :: Eq a => Int -> [a] -> [[a]]
+permut 0 _ = [[]]
+permut n xs = do
+  y <- xs
+  let ys = delete y xs
+  map (y:) $ permut (n - 1) ys
+
 testAns :: Ans -> Ans -> Hint
-testAns xs ys = (hit, temp - hit)
+testAns xs ys = (hit, tmp - hit)
   where
-    hit = length . (filter (== True)) $ zipWith (==) xs ys
-    temp = length [x | x <- xs, y <- ys, x == y]
+    hit = length [x | (x, y) <- zip xs ys, x == y]
+    tmp = length [x | x <- xs, y <- ys, x == y]
+
+testHint :: (Ans, Hint) -> [Ans] -> [Ans]
+testHint (y, h) xs = [x | x <- xs, testAns x y == h]
 
 playRound :: Ans -> IO ()
 playRound x = do
@@ -50,16 +96,38 @@ playRound x = do
   case testAns x y of
     (4, _) -> putStrLn "That's right!"
     (h, b) -> do
-      putStr $ "HIT: " ++ show h ++ ", "
-      putStrLn $ "BLOW: " ++ show b
+      putStr $ "HIT: " ++ show h
+      putStrLn $ ", BLOW: " ++ show b
       putStrLn "Try again!"
       playRound x
 
+cpuRound :: [Ans] -> IO ()
+cpuRound xs = do
+  x <- choiceAns xs
+  putStrLn $ "\nI try " ++ x
+  putStrLn "Tell me your hint."
+  s <- playerHint
+  case s of
+    (4, _) -> putStrLn "I got it!"
+    _ -> case testHint (x, s) xs of
+      [] -> putStrLn "You told me a lie."
+      xs' -> cpuRound xs'
+
 main :: IO ()
 main = do
-  putStrLn "The game is beginning."
-  x <- cpuAns
-  playRound x
+  putStrLn "\nThe game is beginning."
+  putStrLn "Choose game mode 1 or 2."
+  putStrLn "1: Player challenge"
+  putStrLn "2: Computer challenge"
+  mode <- getLine
+  case mode of
+    "1" -> do
+      x <- cpuAns
+      playRound x    
+    "2" -> do
+      let xs = possibleAns
+      cpuRound xs
+    _ -> return ()
   putStrLn "New game? (y/n)"
   yn <- getLine
   if yn == "y" then main else return ()
